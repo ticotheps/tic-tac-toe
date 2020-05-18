ba2dc078b54334af38b068c2e8235dca45504f4d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-18 08:47:22 -0400

    feat: add a 'squares' prop and an 'onClick()' event listener to <Board />

--- src/index.js
+++ src/index.js
@@ -91,7 +91,10 @@ class Game extends React.Component {
 		return (
 			<div className='game'>
 				<div className='game-board'>
-					<Board />
+					<Board
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
 				</div>
 				<div className='game-info'>
 					<div>{/* status */}</div>
