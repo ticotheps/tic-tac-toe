9d9af1a088e9e4e7725d40891a777a435c41e329
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-18 08:45:57 -0400

    feat: add variables to the <Game />'s render() function
    The point of this is to allow the <Game /> to control which version/state of the game to render to the board.

--- src/index.js
+++ src/index.js
@@ -77,6 +77,17 @@ class Game extends React.Component {
 		};
 	}
 	render() {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const winner = calculateWinner(current.squares);
+		let status;
+
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		}
+
 		return (
 			<div className='game'>
 				<div className='game-board'>
