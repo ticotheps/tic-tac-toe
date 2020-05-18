26dacac8ec10aac900dc3a25de467487aaed9482
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-18 09:14:01 -0400

    feat: modify the 'history' value
    We want to modify this value to only return the history associated with the stepNumber that the user is viewing.

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ class Game extends React.Component {
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
