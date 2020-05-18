3b2ad39e4ffff46ccfde7cc24de76428f5318ee1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-18 07:38:37 -0400

    refactor: replace 'this.state.value' with 'this.props.value'
    This will happen in the <Square /> component's render() method

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class Square extends React.Component {
 				className='square'
 				onClick={() => this.setState({ value: 'X' })}
 			>
-				{this.state.value}
+				{this.props.value}
 			</button>
 		);
 	}
